d87c5e6a1b8d2f2b40cfb08d6874a4f9a74ad4e2
Leandro Kamada <132115451+LeandroKamadaa@users.noreply.github.com>
2026-08-07 10:12:59 -0300

    aaa

---
+++ 06-07-26/MeuPrimeiroApp/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Qual seu nome?");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Qual sua idade?");
+        string idade = Console.ReadLine();
+
+        Console.WriteLine("Qual seu curso?");
+        string curso = Console.ReadLine();
+
+        Console.WriteLine($"Ola {nome}, {idade} anos do curso de {curso}");
+    }
+}
