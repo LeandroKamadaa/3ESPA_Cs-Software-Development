91372b7e6970614865fa9bbbd2d2c7ca58a0c0f4
Leandro Kamada <132115451+LeandroKamadaa@users.noreply.github.com>
2026-08-10 11:29:49 -0300

    aaa

---
+++ 08-10-26/Tipos.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main)
+    {
+        Console.WriteLine("Qual seu nome?");
+        string nome = Console.ReadLine();
+        
+        Console.writeLine("Qual sua idade?");
+        int idade = Console.readLine();
+
+        Console.WriteLine("Qual sua altura?");
+        double altura = Console.ReadLine();
+
+        Console.WriteLine("Qual seu peso?");
+        double peso = Console.ReadLine();
+
+        Console.WriteLine("Qual sua cidade?");
+        var cidade = Console.ReadLine();
+        
+        Console.WriteLine($"Ola {nome}, {idade} anos, {altura}m de altura, {peso}kg, da cidade de {cidade}");
+        
+        string msg = $"Informações do nome{nome.Trim()}"
+        console.WriteLine(msg);
+
+    }
+}
